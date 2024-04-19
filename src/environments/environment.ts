37133a1df806f2fad6c2e38b92765049fff02bf0
“atenoc” <car.ateno@gmail.com>
2024-04-19 13:44:24 -0600

    Primer commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,16 @@
+export const environment = {
+  production: false,
+  firebase:{
+
+    apiKey: "AIzaSyAU3oDso74G-39MaomYMG61GMaEOB2Li_M",
+    authDomain: "prueba-ed0d7.firebaseapp.com",
+    databaseURL: "https://prueba-ed0d7.firebaseio.com",
+    projectId: "prueba-ed0d7",
+    storageBucket: "prueba-ed0d7.appspot.com",
+    messagingSenderId: "821982309063",
+    appId: "1:821982309063:web:a45e874f8b8dd1f5132ce2"
+
+}
+};
+
+//Esto lo encontraran en clase Agregar AngularFire2 del proyecto final
